d39ea4b29dadda0e59d181aef65313a8904652c7
Patrick Todd <patrick.a.todd@gmail.com>
2016-08-31 11:10:43 -0400

    added routing for nasa page

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,9 +1,13 @@
-var app = angular.module('app', ['ngRoute', 'ngCookies', 'ngMaterial', 'ngMessages']);
+var app = angular.module('app', ['ngRoute', 'ngCookies', 'ngMaterial', 'ngMessages', 'ngParallax']);
 app.config(function($routeProvider){
 $routeProvider
 	.when('/', {
-		templateUrl: 'partials/photoPartial.html',
-		controller: 'photoController'
+		templateUrl: 'partials/mainPartial.html',
+		controller: 'mainController'
+	})
+	.when('/nasa', {
+		templateUrl: 'partials/nasaPartial.html',
+		controller: 'nasaController'
 	})
 	.otherwise({
 			redirectTo: '/'
