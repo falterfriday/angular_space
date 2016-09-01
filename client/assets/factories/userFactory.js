4f715765071097abec6c9d43d3fd56b83bb6c442
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-01 12:20:56 -0400

    user factory updated with registration method

--- client/assets/factories/userFactory.js
+++ client/assets/factories/userFactory.js
@@ -2,8 +2,8 @@ app.factory('userFactory', ['$http', function($http){
 
 	function userFactory(){
 		this.register = function(newUser, callback){
-			console.log("hit the uF - register method");
-			$http.post('/register', newUser).then(function(rtnData){
+			console.log("uF - newUser = ", newUser);
+			$http.post('/create_user', newUser).then(function(rtnData){
 				console.log("uF rtnData = ", rtnData.data);
 				if(typeof(callback) == "function"){
 					callback(rtnData.data);
