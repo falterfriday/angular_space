b46e6a9c86dd7d60dfb57dce605ef6eb19a9908e
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-01 12:26:27 -0400

    updated model with validations - still not displaying though

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,55 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 
+var validateEmail = function(email){
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 var UserSchema = new mongoose.Schema({
-  name: {
+  first_name: {
     type: String,
     required: [true, "name required"],
     trim: true,
     minlength: [2, "too short"]
   },
-  id: {
-    type: String
-  }
+  last_name: {
+    type: String,
+    required: [true, "name required"],
+    trim: true,
+    minlength: [2, "too short"]
+  },
+  email: {
+    type: String,
+    unique:true,
+    lowercase: true,
+    required: [true, "email required"],
+    validate: [validateEmail, "invalid email"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "invalid email"]
+  },
+  password: {
+    type:String,
+    required: [true, "password required"],
+    minlength: [8, "password too short"],
+    maxlength: [32, "password too long"],
+    validate: {
+      validator: function(value){
+        var pattern = new RegExp(value);
+        return pattern.test(this.pw_conf);
+      },
+      message: "passwords must match"
+    }
+  },
+  pw_conf: {
+    type: String,
+    required: [true, "confirmation required"]
+  },
 }, {timestamps:true});
 
-
+UserSchema.pre('save', function(done){
+  this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  this.pw_conf = '';
+  done();
+});
 
 var User = mongoose.model('User', UserSchema);
