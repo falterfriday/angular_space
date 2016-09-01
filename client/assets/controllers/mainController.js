77064e6b9fc24509f2d25e8fdb5fe48ffb853eab
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-01 12:16:55 -0400

    main controller updated with conditional cookies check

--- client/assets/controllers/mainController.js
+++ client/assets/controllers/mainController.js
@@ -1,11 +1,14 @@
 app.controller('mainController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', function(photoFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http){
-
+	if($cookies.getObject('user')){
+		$scope.user = $cookies.getObject('user');
+		console.log('current user = ', $scope.user);
+	}
 	$scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
+	$scope.toggleRight = buildToggler('right');
 
-    function buildToggler(componentId) {
-      return function() {
-        $mdSidenav(componentId).toggle();
-			};
-    }
+	function buildToggler(componentId) {
+		return function() {
+			$mdSidenav(componentId).toggle();
+		};
+	}
 }]);
