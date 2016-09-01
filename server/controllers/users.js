5c0d4c0b2c260397dfbfb8c709cf1d38b9afc1f4
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-01 12:25:42 -0400

    updated users to save user registration details

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,14 @@ var User = mongoose.model('User');
 
 function UserController(){
   this.createUser = function(req,res){
-    var user = User({name: req.body.name});
+    var user = User({
+      first_name: req.body.first_name,
+      last_name:req.body.last_name,
+      email:req.body.email,
+      password:req.body.password,
+      pw_conf:req.body.pw_conf
+    });
+    console.log("users.js = ", user);
     user.save(function(err){
       if(err){
         console.log('new user not added');
