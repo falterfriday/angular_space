1348d785fb28b5c45fdc365ede407f90330e0006
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-02 08:44:14 -0400

    added controller for reddit content

---
+++ client/assets/controllers/redditController.js
@@ -0,0 +1,16 @@
+app.controller('redditController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', function(mainFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http){
+
+  $scope.getPhotos = function(){
+    $http.get('https://www.reddit.com/r/spaceporn/.json?').then(function(returnedData){
+      $scope.photos = [];
+      // console.log(returnedData.data.data.children);
+      // $scope.photos = returnedData.data.data.children;
+      for (var x = 0; x <= 10; x++){
+        $scope.photos.push(returnedData.data.data.children[x]);
+      }
+      console.log($scope.photos);
+    });
+  };
+  $scope.getPhotos();
+
+}]);
