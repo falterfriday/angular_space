a33b9a100aa0c7f835a1acee3a9881aa05bd3e76
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-02 08:47:01 -0400

    completely refactored to pull needed dates and apis

--- client/assets/controllers/nasaController.js
+++ client/assets/controllers/nasaController.js
@@ -1,53 +1,19 @@
 app.controller('nasaController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', function(mainFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http){
-	// console.log('nasa controller');
 
-
-//- - - - - - - -refactor this- - - - - - - - A LOT- - - - - - - -
 	$scope.getPhotos = function(){
-		var date = new Date();
-		var today = date.toISOString().substring(0, 10);
-		var yesterday = new Date((new Date()).valueOf() - 1000*60*60*24);
-		yesterday = yesterday.toISOString().substring(0, 10);
-		var twoDaysAgo = new Date((new Date()).valueOf() - 1000*60*60*24*2);
-		twoDaysAgo = twoDaysAgo.toISOString().substring(0, 10);
-		var threeDaysAgo = new Date((new Date()).valueOf() - 1000*60*60*24*3);
-		threeDaysAgo = threeDaysAgo.toISOString().substring(0, 10);
-		$http.get('https://api.nasa.gov/planetary/apod?hd=True&date=' +today+ '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB')
-		.success(function(rtnData){
-			$scope.photo = rtnData;
-			console.log('rtnData = ', rtnData);
-		})
-		.error(function(error, status, headers, config){
-			console.log(status);
-			console.log("Error retrieving image");
-		});
-		$http.get('https://api.nasa.gov/planetary/apod?hd=True&date=' +yesterday+ '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB')
-		.success(function(rtnData){
-			$scope.photo2 = rtnData;
-			console.log('rtnData = ', rtnData);
-		})
-		.error(function(error, status, headers, config){
-			console.log(status);
-			console.log("Error retrieving image");
-		});
-		$http.get('https://api.nasa.gov/planetary/apod?hd=True&date=' +twoDaysAgo+ '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB')
-		.success(function(rtnData){
-			$scope.photo3 = rtnData;
-			console.log('rtnData = ', rtnData);
-		})
-		.error(function(error, status, headers, config){
-			console.log(status);
-			console.log("Error retrieving image");
-		});
-		$http.get('https://api.nasa.gov/planetary/apod?hd=True&date=' +threeDaysAgo+ '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB')
-		.success(function(rtnData){
-			$scope.photo4 = rtnData;
-			console.log('rtnData = ', rtnData);
-		})
-		.error(function(error, status, headers, config){
-			console.log(status);
-			console.log("Error retrieving image");
-		});
+		$scope.apiUrl = [];
+		$scope.returnedDataArr = [];
+		//loop sets date yesterday - 9 days ago
+		for (var x = 1; x <= 10; x++){
+			$scope.date = (new Date((new Date()).valueOf() - 1000*60*60*24* x )).toISOString().substring(0, 10);
+			$scope.apiUrl.push('https://api.nasa.gov/planetary/apod?hd=True&date=' +$scope.date+ '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB');
+		}
+		// loop grabs the url from the photo array and gets the associated data
+		for (var y = 0; y < $scope.apiUrl.length; y++){
+			$http.get( $scope.apiUrl[y] ).then( function(returnedData){
+				$scope.returnedDataArr.push(returnedData.data);
+			});
+		}
 	};
 	$scope.getPhotos();
 }]);
