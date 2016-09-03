22d52ece15805cf72d6c62bd05569cf86b7ec0df
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-03 13:00:25 -0400

    added routing for login, addfav, getUser, and getUserFav

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,4 +4,8 @@ var users = require('../controllers/users.js');
 
 module.exports = function(app){
 	app.post('/create_user', users.createUser);
+	app.post('/login', users.loginUser);
+	app.post('/addFavorite', users.addFavorite);
+	app.post('/getUserInfo', users.getUserInfo);
+	app.post('/getUserFavorites', users.getUserFavorites);
 };
