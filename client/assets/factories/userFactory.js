2a0329b53fb8bfc78e713a1285d7938601f46097
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-03 12:53:39 -0400

    modified naming for readability, added login/add favorite/ get user methods

--- client/assets/factories/userFactory.js
+++ client/assets/factories/userFactory.js
@@ -1,13 +1,32 @@
 app.factory('userFactory', ['$http', function($http){
 
 	function userFactory(){
-		this.register = function(newUser, callback){
+		this.registerUser = function(newUser, callback){
 			console.log("uF - newUser = ", newUser);
-			$http.post('/create_user', newUser).then(function(rtnData){
-				console.log("uF rtnData = ", rtnData.data);
+			$http.post('/create_user', newUser).then(function(returnedData){
+				console.log("uF returnedData = ", returnedData.data);
 				if(typeof(callback) == "function"){
-					callback(rtnData.data);
+					callback(returnedData.data);
 				}
+			});
+		};
+		this.loginUser = function(existingUser, callback){
+			console.log("uF - loginUser = ", existingUser);
+			$http.post('/login', existingUser).then(function(returnedData){
+				console.log("uF - returnedData = ", returnedData.data);
+				callback(returnedData.data);
+			});
+		};
+		this.addFavorite = function(favorite, callback){
+			console.log("uF - favorite = ", favorite);
+			$http.post('/addFavorite', favorite).then(function(returnedData){
+				callback(returnedData);
+			});
+		};
+		this.getUserFavorites = function(user, callback){
+			console.log('uF - getUserFavorites', user);
+			$http.post('/getUserFavorites', user).then(function(returnedData){
+				callback(returnedData);
 			});
 		};
 	}
