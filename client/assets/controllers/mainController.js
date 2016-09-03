5d6e0a15892881ac18be95b59576cb27df4482ba
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-03 12:49:00 -0400

    added logout functionality and conditional check for user cookies

--- client/assets/controllers/mainController.js
+++ client/assets/controllers/mainController.js
@@ -1,13 +1,21 @@
-app.controller('mainController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', function(photoFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http){
+app.controller('mainController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$window', function(mainFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $window){
 	if($cookies.getObject('user')){
 		$scope.user = $cookies.getObject('user');
 		console.log('current user = ', $scope.user);
+	} else {
+		$scope.user = null;
+		console.log('no current user data');
 	}
 	$scope.toggleLeft = buildToggler('left');
 	$scope.toggleRight = buildToggler('right');
 
-	function buildToggler(componentId) {
-		return function() {
+	$scope.logoutUser = function(){
+		console.log("logout clicked");
+		$cookies.remove('user');
+		$window.location.reload();
+	};
+	function buildToggler(componentId){
+		return function(){
 			$mdSidenav(componentId).toggle();
 		};
 	}
