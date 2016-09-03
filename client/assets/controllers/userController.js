81cb5c514eae7b2fc25f4a9fb537ff194d4339df
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-03 12:52:23 -0400

    refactored user registration, added user login, and set cookies for both

--- client/assets/controllers/userController.js
+++ client/assets/controllers/userController.js
@@ -1,6 +1,5 @@
-app.controller('userController', ['userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog', function(userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog){
-	// console.log("userController");
-	$scope.status = '  ';
+app.controller('userController', ['userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog', '$window', function(userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog, $window){
+	// $scope.status = '  ';
   $scope.customFullscreen = false;
 
   $scope.showLoginDialog = function(ev) {
@@ -11,30 +10,38 @@ app.controller('userController', ['userFactory', '$scope', '$location', '$cookie
       targetEvent: ev,
       clickOutsideToClose:false,
       fullscreen: $scope.customFullscreen
-    })
-    .then(function(answer) {
-      // $scope.status = 'You said the information was "' + answer + '".';
-    }, function() {
-      // $scope.status = 'You cancelled the dialog.';
     });
   };
+  // ----------------controller-ception?!----------------
   function LoginController($scope, $mdDialog) {
     $scope.loginUser = function(){
-      console.log($scope.login);
+      console.log($scope.existingUser);
+      userFactory.loginUser($scope.existingUser, function(returnedData){
+        if(returnedData.error){
+          console.log('login unsucessful');
+          $scope.login = {};
+        } else {
+          $cookies.putObject('user',{first_name:returnedData.first_name, last_name:returnedData.last_name, id:returnedData._id});
+          $scope.cancel();
+          $window.location.reload();
+        }
+      });
       $scope.login = {};
     };
     $scope.registerUser = function(){
       console.log(" uC newUser = ", $scope.newUser);
-      userFactory.register($scope.newUser, function(rtnData){
-        if(rtnData.error){
-          console.log(rtnData.error);
-          $scope.errors = rtnData;
+      userFactory.registerUser($scope.newUser, function(returnedData){
+        if(returnedData.error){
+          console.log(returnedData.error);
+          $scope.errors = returnedData;
           $scope.newUser = {};
         } else {
-    			$cookies.putObject('user',{first_name:rtnData.first_name, last_name:rtnData.last_name});
-          $location.url('/');
+    			$cookies.putObject('user',{first_name:returnedData.first_name, last_name:returnedData.last_name, id:returnedData._id});
+          console.log('current user = ', $scope.user);
+          $scope.cancel();
+          $window.location.reload();
         }
-		});
+	    });
     };
     $scope.cancel = function() {
       $mdDialog.cancel();
