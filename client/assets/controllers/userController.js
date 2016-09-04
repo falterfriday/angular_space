a9e24dd88eb5f3a99bbf457a3e3135cdf2903984
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-04 12:00:02 -0400

    added checkUserState and loginController

--- client/assets/controllers/userController.js
+++ client/assets/controllers/userController.js
@@ -1,6 +1,16 @@
 app.controller('userController', ['userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog', '$window', function(userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog, $window){
-	// $scope.status = '  ';
+
   $scope.customFullscreen = false;
+
+  $scope.checkUserState = function(){
+		if($cookies.getObject('user')){
+			$scope.user = $cookies.getObject('user');
+			console.log('current user = ', $scope.user);
+		} else {
+			$scope.user = null;
+			console.log('no current user data');
+		}
+	};
 
   $scope.showLoginDialog = function(ev) {
     $mdDialog.show({
@@ -14,16 +24,26 @@ app.controller('userController', ['userFactory', '$scope', '$location', '$cookie
   };
   // ----------------controller-ception?!----------------
   function LoginController($scope, $mdDialog) {
+    $scope.checkUserState = function(){
+  		if($cookies.getObject('user')){
+  			$scope.user = $cookies.getObject('user');
+  			console.log('current user = ', $scope.user);
+  		} else {
+  			$scope.user = null;
+  			console.log('no current user data');
+  		}
+  	};
     $scope.loginUser = function(){
       console.log($scope.existingUser);
       userFactory.loginUser($scope.existingUser, function(returnedData){
-        if(returnedData.error){
+        if(returnedData.errors){
           console.log('login unsucessful');
           $scope.login = {};
         } else {
           $cookies.putObject('user',{first_name:returnedData.first_name, last_name:returnedData.last_name, id:returnedData._id});
+          // $window.location.reload();
+          $scope.checkUserState();
           $scope.cancel();
-          $window.location.reload();
         }
       });
       $scope.login = {};
@@ -31,7 +51,8 @@ app.controller('userController', ['userFactory', '$scope', '$location', '$cookie
     $scope.registerUser = function(){
       console.log(" uC newUser = ", $scope.newUser);
       userFactory.registerUser($scope.newUser, function(returnedData){
-        if(returnedData.error){
+        console.log('returnedData = ', returnedData);
+        if(returnedData.errors){
           console.log(returnedData.error);
           $scope.errors = returnedData;
           $scope.newUser = {};
@@ -39,7 +60,8 @@ app.controller('userController', ['userFactory', '$scope', '$location', '$cookie
     			$cookies.putObject('user',{first_name:returnedData.first_name, last_name:returnedData.last_name, id:returnedData._id});
           console.log('current user = ', $scope.user);
           $scope.cancel();
-          $window.location.reload();
+          // $window.location.reload();
+          $scope.checkUserState();
         }
 	    });
     };
