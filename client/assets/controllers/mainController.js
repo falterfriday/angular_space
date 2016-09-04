ec448bcddb10aafd006c1ca4f374f2596447a176
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-04 11:54:29 -0400

    added callable function around check user state

--- client/assets/controllers/mainController.js
+++ client/assets/controllers/mainController.js
@@ -1,18 +1,23 @@
 app.controller('mainController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$window', function(mainFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $window){
-	if($cookies.getObject('user')){
-		$scope.user = $cookies.getObject('user');
-		console.log('current user = ', $scope.user);
-	} else {
-		$scope.user = null;
-		console.log('no current user data');
-	}
+	$scope.checkUserState = function(){
+		if($cookies.getObject('user')){
+			$scope.user = $cookies.getObject('user');
+			console.log('current user = ', $scope.user);
+		} else {
+			$scope.user = null;
+			console.log('no current user data');
+		}
+	};
+	$scope.checkUserState();
+
 	$scope.toggleLeft = buildToggler('left');
 	$scope.toggleRight = buildToggler('right');
 
 	$scope.logoutUser = function(){
 		console.log("logout clicked");
 		$cookies.remove('user');
-		$window.location.reload();
+		// $window.location.reload();
+		$scope.checkUserState();
 	};
 	function buildToggler(componentId){
 		return function(){
