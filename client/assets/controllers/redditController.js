e31f79a49480d5781b235fa35d0129bb41368f59
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-04 11:58:21 -0400

    added addFav and showPhoto methods

--- client/assets/controllers/redditController.js
+++ client/assets/controllers/redditController.js
@@ -1,4 +1,4 @@
-app.controller('redditController', ['mainFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', function(mainFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http){
+app.controller('redditController', ['mainFactory','userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog' , function(mainFactory, userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog){
 
   $scope.getPhotos = function(){
     $http.get('https://www.reddit.com/r/spaceporn/.json?').then(function(returnedData){
@@ -11,4 +11,39 @@ app.controller('redditController', ['mainFactory', '$scope', '$location', '$cook
   };
   $scope.getPhotos();
 
+  $scope.addFavorite = function(photoInfo){
+		$scope.favorite = photoInfo.data;
+		$scope.favorite.userId = $scope.user.id;
+		console.log("favorite = ", $scope.favorite);
+		userFactory.addRedditFavorite($scope.favorite, function(returnedData){
+			console.log(returnedData);
+		});
+	};
+
+//---------------------------OPEN PHOTO TAB---------------------------
+  $scope.showPhoto = function(ev, clickedPhoto) {
+		// $scope.clickedPhoto = clickedPhoto;
+		console.log("photo clicked");
+		console.log("linked content = ", clickedPhoto);
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '../../partials/photoPartial.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    });
+		function DialogController($scope, $mdDialog){
+			$scope.clickedPhoto = clickedPhoto;
+			$scope.hide = function() {
+				$mdDialog.hide();
+			};
+			$scope.cancel = function() {
+				$mdDialog.cancel();
+			};
+			$scope.answer = function(answer) {
+				$mdDialog.hide(answer);
+			};
+		}
+  };
 }]);
