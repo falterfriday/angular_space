40cc095af8a6e96189a7142a63331e27ed5fba5a
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-05 08:20:37 -0400

    commented out checkUserState due to login issues, pulling 20 photos instead of 10

--- client/assets/controllers/redditController.js
+++ client/assets/controllers/redditController.js
@@ -1,9 +1,24 @@
-app.controller('redditController', ['mainFactory','userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog' , function(mainFactory, userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog){
+app.controller('redditController', ['userFactory', '$scope', '$location', '$cookies', '$routeParams', '$timeout', '$mdSidenav', '$http', '$mdDialog' , function(userFactory, $scope, $location, $cookies, $routeParams, $timeout, $mdSidenav, $http, $mdDialog){
 
+//----------------------CHECK LOGIN STATUS----------------------
+  // $scope.checkUserState = function(){
+	// 	if($cookies.getObject('user')){
+	// 		$scope.user = $cookies.getObject('user');
+  //     $scope.userLoggedIn = true;
+	// 		console.log('user = ', $scope.user);
+	// 	} else {
+	// 		// $scope.user = null;
+  //     $scope.userLoggedIn = false;
+	// 		console.log('no current user data');
+	// 	}
+	// };
+	// $scope.checkUserState();
+
+//------------------------GRAB 20 MOST RECENT POSTS ON R/SPACEPORN------------------------
   $scope.getPhotos = function(){
     $http.get('https://www.reddit.com/r/spaceporn/.json?').then(function(returnedData){
       $scope.photos = [];
-      for (var x = 0; x <= 10; x++){
+      for (var x = 0; x <= 20; x++){
         $scope.photos.push(returnedData.data.data.children[x]);
       }
       console.log($scope.photos);
@@ -11,8 +26,11 @@ app.controller('redditController', ['mainFactory','userFactory', '$scope', '$loc
   };
   $scope.getPhotos();
 
+//-----------------------ADD FAV WHEN HEART IS CLICKED-----------------------
   $scope.addFavorite = function(photoInfo){
 		$scope.favorite = photoInfo.data;
+		$scope.checkUserState();
+		console.log("user = ", $scope.user);
 		$scope.favorite.userId = $scope.user.id;
 		console.log("favorite = ", $scope.favorite);
 		userFactory.addRedditFavorite($scope.favorite, function(returnedData){
@@ -20,20 +38,19 @@ app.controller('redditController', ['mainFactory','userFactory', '$scope', '$loc
 		});
 	};
 
-//---------------------------OPEN PHOTO TAB---------------------------
+//---------------------------OPEN PHOTO DIALOG---------------------------
   $scope.showPhoto = function(ev, clickedPhoto) {
-		// $scope.clickedPhoto = clickedPhoto;
 		console.log("photo clicked");
 		console.log("linked content = ", clickedPhoto);
     $mdDialog.show({
-      controller: DialogController,
-      templateUrl: '../../partials/photoPartial.html',
+      controller: photoController,
+      templateUrl: '../../partials/redditPhotoPartial.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true,
-      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      fullscreen: $scope.customFullscreen
     });
-		function DialogController($scope, $mdDialog){
+		function photoController($scope, $mdDialog){
 			$scope.clickedPhoto = clickedPhoto;
 			$scope.hide = function() {
 				$mdDialog.hide();
