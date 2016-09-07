19173cb5b2871438f2832fe91270416ceb2ad24d
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-07 18:55:48 -0400

    commenting for future fixes

--- server/models/favorite.js
+++ server/models/favorite.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//need to pull the userID into favorite
+// query favorites with ID and see if the URL already exists
 var FavoriteSchema = new mongoose.Schema({
   url: {
-    type: String
+    type: String,
+    unique:true,
   },
   hdUrl: {
     type: String
