386591c26393221431d6e80018bd40ede610b2fc
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-13 19:23:09 -0400

    altered the info being passed to the back-end when saving a favorite

--- client/assets/factories/userFactory.js
+++ client/assets/factories/userFactory.js
@@ -2,9 +2,9 @@ app.factory('userFactory', ['$http', function($http){
 
 	function userFactory(){
 		this.registerUser = function(newUser, callback){
-			console.log("uF - newUser = ", newUser);
+			// console.log("uF - newUser = ", newUser);
 			$http.post('/create_user', newUser).then(function(returnedData){
-				console.log("uF returnedData = ", returnedData.data);
+				// console.log("uF returnedData = ", returnedData.data);
 				if(typeof(callback) == "function"){
 					callback(returnedData.data);
 				}
@@ -18,20 +18,26 @@ app.factory('userFactory', ['$http', function($http){
 			});
 		};
 		this.addNasaFavorite = function(favorite, callback){
-			console.log("uF - favorite = ", favorite);
+			// console.log("uF - favorite = ", favorite);
 			$http.post('/addNasaFavorite', favorite).then(function(returnedData){
 				callback(returnedData);
 			});
 		};
 		this.addRedditFavorite = function(favorite, callback){
-			console.log("uF - favorite = ", favorite);
+			// console.log("uF - favorite = ", favorite);
 			$http.post('/addRedditFavorite', favorite).then(function(returnedData){
 				callback(returnedData);
 			});
 		};
 		this.getUserFavorites = function(user, callback){
-			console.log('uF - getUserFavorites', user);
+			// console.log('uF - getUserFavorites', user);
 			$http.post('/getUserFavorites', user).then(function(returnedData){
+				callback(returnedData);
+			});
+		};
+		this.deleteFavorite = function(favorite, callback){
+			console.log('uF delete');
+			$http.post('/deleteFavorite', favorite).then(function(returnedData){
 				callback(returnedData);
 			});
 		};
