79ead8fba113023ef80092ae7e673c3a8c08afe5
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-30 10:47:23 -0400

    added getFavUrls and deleteFavorite routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,10 @@ var users = require('../controllers/users.js');
 module.exports = function(app){
 	app.post('/create_user', users.createUser);
 	app.post('/login', users.loginUser);
+	app.post('/getFavUrls', users.getFavUrls);
 	app.post('/addNasaFavorite', users.addNasaFavorite);
 	app.post('/addRedditFavorite', users.addRedditFavorite);
 	app.post('/getUserInfo', users.getUserInfo);
 	app.post('/getUserFavorites', users.getUserFavorites);
+	app.post('/deleteFavorite', users.deleteFavorite);
 };
