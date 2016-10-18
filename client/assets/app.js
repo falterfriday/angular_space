25b29849bcf202bb1e830ce008695cbcf6756d7e
Patrick Todd <patrick.a.todd@gmail.com>
2016-10-18 12:03:30 -0400

    refactoring style and syntax for readability

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,10 +1,22 @@
-var app = angular.module('app', ['ngRoute', 'ngCookies', 'ngMaterial', 'ngMessages', '720kb.socialshare']);
-app.config(function($mdThemingProvider) {
+angular
+	.module('app', [
+		'ngRoute',
+		'ngCookies',
+		'ngMaterial',
+		'ngMessages',
+		'720kb.socialshare'
+	]);
+
+angular
+	.module('app')
+	.config(function($mdThemingProvider) {
 	$mdThemingProvider.theme('default')
 	.dark();
 });
 
-app.config(function($routeProvider){
+angular
+	.module('app')
+	.config(function($routeProvider){
 $routeProvider
 	.when('/', {
 		templateUrl: 'partials/mainPartial.html',
