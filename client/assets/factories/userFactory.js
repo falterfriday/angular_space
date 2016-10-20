541967e7245ca87f83edc9743e806d8724ec9345
Patrick Todd <patrick.a.todd@gmail.com>
2016-10-20 18:50:00 -0400

    refactoring for come consistent industry standards

--- client/assets/factories/userFactory.js
+++ client/assets/factories/userFactory.js
@@ -1,63 +1,86 @@
+/* jshint esversion: 6 */
 angular
 	.module('app')
-	.factory('userFactory', ['$http', function($http){
+	.factory('userFactory',userFactory);
 
-	function userFactory(){
-		this.registerUser = function(newUser, callback){
-			// console.log("uF - newUser = ", newUser);
-			$http.post('/create_user', newUser).then(function(returnedData){
-				// console.log("uF returnedData = ", returnedData.data);
-				if(typeof(callback) == "function"){
-					callback(returnedData.data);
-				}
-			});
-		};
-		this.loginUser = function(existingUser, callback){
-			// console.log("uF - loginUser = ", existingUser);
-			$http.post('/login', existingUser).then(function(returnedData){
-				// console.log("uF - returnedData = ", returnedData.data);
+userFactory.$inject = ['$http'];
+
+function userFactory($http){
+
+	return {
+		registerUser: registerUser,
+		loginUser: loginUser,
+		addNasaFavorite: addNasaFavorite,
+		addRedditFavorite: addRedditFavorite,
+		getFavUrls: getFavUrls,
+		getUserFavorites: getUserFavorites,
+		deleteFavorite: deleteFavorite
+	};
+
+///////////////////////////////////////////////////////////////////////
+
+	function registerUser(newUser, callback){
+		// console.log("uF - newUser = ", newUser);
+		$http.post('/create_user', newUser).then(function(returnedData){
+			// console.log("uF returnedData = ", returnedData.data);
+			if(typeof(callback) == "function"){
 				callback(returnedData.data);
-			});
-		};
-		this.addNasaFavorite = function(favorite, callback){
-			// console.log("uF - favorite = ", favorite);
-			$http.post('/addNasaFavorite', favorite).then(function(returnedData){
-				callback(returnedData);
-			});
-		};
-		this.addRedditFavorite = function(favorite, callback){
-			// console.log("uF - favorite = ", favorite);
-			$http.post('/addRedditFavorite', favorite).then(function(returnedData){
-				callback(returnedData);
-			});
-		};
-		this.getUserFavorites = function(user, callback){
-			// console.log('uF - getUserFavorites', user);
-			$http.post('/getUserFavorites', user).then(function(returnedData){
-				callback(returnedData);
-			});
-		};
-		this.getFavUrls = function(user, callback){
-			console.log("user = ", user);
-			$http.post('/getFavUrls', user).then(function(returnedData){
-				console.log("uF returnedData = ", returnedData);
-				var arrFavs = [];
-				if (returnedData.data.length > 0){
-					favoritesData = returnedData.data[0]._favorites;
-					for (var i = 0; i < favoritesData.length ; i++){
-						arrFavs.push(favoritesData[i].url);
-					}
-				}
-				// console.log("userFactory returnedData = ", favoritesData);
-				callback(arrFavs);
-			});
-		};
-		this.deleteFavorite = function(favorite, callback){
-			console.log('uF delete', favorite);
-			$http.post('/deleteFavorite', favorite).then(function(returnedData){
-				callback(returnedData);
-			});
-		};
+			}
+		});
 	}
-	return new userFactory();
-}]);
+	function loginUser(existingUser, callback){
+		// console.log("uF - loginUser = ", existingUser);
+		$http.post('/login', existingUser).then(function(returnedData){
+			// console.log("uF - returnedData = ", returnedData.data);
+			callback(returnedData.data);
+		});
+	}
+	function addNasaFavorite(favorite, callback){
+		// console.log("uF - favorite = ", favorite);
+		$http.post('/addNasaFavorite', favorite).then(function(returnedData){
+			callback(returnedData);
+		});
+	}
+	function addRedditFavorite(favorite, callback){
+		// console.log("uF - favorite = ", favorite);
+		$http.post('/addRedditFavorite', favorite).then(function(returnedData){
+			callback(returnedData);
+		});
+	}
+	function getUserFavorites(user){
+		console.log("uF user = ", user);
+		return $http.post('/getUserFavorites', user)
+			.then(getDataSuccess)
+			.catch(getDataFailed);
+
+		function getDataSuccess(returnedData){
+			console.log("returnedData = ", returnedData);
+			return returnedData;
+		}
+		function getDataFailed(error){
+			logger.error(`Failed to retrieve user favorites. ${error.data}`);
+		}
+	}
+
+	function getFavUrls(user, callback){
+		console.log("user = ", user);
+		$http.post('/getFavUrls', user).then(function(returnedData){
+			console.log("uF returnedData = ", returnedData);
+			var arrFavs = [];
+			if (returnedData.data.length > 0){
+				favoritesData = returnedData.data[0]._favorites;
+				for (var i = 0; i < favoritesData.length ; i++){
+					arrFavs.push(favoritesData[i].url);
+				}
+			}
+			// console.log("userFactory returnedData = ", favoritesData);
+			callback(arrFavs);
+		});
+	}
+	function deleteFavorite(favorite, callback){
+		console.log('uF delete', favorite);
+		$http.post('/deleteFavorite', favorite).then(function(returnedData){
+			callback(returnedData);
+		});
+	}
+}
