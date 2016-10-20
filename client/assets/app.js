55d2aa107c01889b84185410917e9fdf3b6cdf8a
Patrick Todd <patrick.a.todd@gmail.com>
2016-10-20 18:47:50 -0400

    refactoring for come consistent industry standards

--- client/assets/app.js
+++ client/assets/app.js
@@ -5,36 +5,35 @@ angular
 		'ngMaterial',
 		'ngMessages',
 		'720kb.socialshare'
-	]);
+	])
+	.config(config);
+
+function config($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'partials/mainPartial.html',
+			controller: 'mainController'
+		})
+		.when('/nasa', {
+			templateUrl: 'partials/nasaPartial.html',
+			controller: 'nasaController'
+		})
+		.when('/reddit', {
+			templateUrl: 'partials/redditPartial.html',
+			controller: 'redditController'
+		})
+		.when('/profile/:id', {
+			templateUrl: 'partials/profilePartial.html',
+			controller: 'profileController'
+		})
+		.otherwise({
+				redirectTo: '/'
+		});
+}
 
 angular
 	.module('app')
 	.config(function($mdThemingProvider) {
-	$mdThemingProvider.theme('default')
-	.dark();
-});
-
-angular
-	.module('app')
-	.config(function($routeProvider){
-$routeProvider
-	.when('/', {
-		templateUrl: 'partials/mainPartial.html',
-		controller: 'mainController'
-	})
-	.when('/nasa', {
-		templateUrl: 'partials/nasaPartial.html',
-		controller: 'nasaController'
-	})
-	.when('/reddit', {
-		templateUrl: 'partials/redditPartial.html',
-		controller: 'redditController'
-	})
-	.when('/profile/:id', {
-		templateUrl: 'partials/profilePartial.html',
-		controller: 'profileController'
-	})
-	.otherwise({
-			redirectTo: '/'
-	});
+		$mdThemingProvider.theme('default')
+		.dark();
 });
