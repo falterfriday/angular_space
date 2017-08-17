4f59151d650fb3325ca822fee2d23b4dfc639667
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-16 21:57:19 -0400

    migrated getphotos to photofactory and refactored

--- client/assets/factories/photoFactory.js
+++ client/assets/factories/photoFactory.js
@@ -12,29 +12,34 @@ function photoFactory($http){
         getRedditPhotos: getRedditPhotos
     };
 
+    //NASA APOD API CALL FOR THE LAST 10 DAYS
     function getNasaPhotos(){
-        console.log("hit the photoFactory")
         var returnedDataArr = [];
         var apiUrlString;
         var date;
 
         for (var x = 1; x <= 10; x++){
-            date = (new Date( new Date() - 1000*60*60*24* x ).toISOString()).substring(0,10);            
+            date = (new Date( new Date() - 1000*60*60*24* x )
+            .toISOString())
+            .substring(0,10);            
             apiUrlString = 'https://api.nasa.gov/planetary/apod?hd=True&date=' + date + '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB';
-            $http.get(apiUrlString).then(function(returnedData){
+            $http.get(apiUrlString)
+            .then(function(returnedData){
                 returnedDataArr.push(returnedData.data);
             });
         }
-        console.log("returnedDataArr from photoFactory");
         return returnedDataArr;
     }
 
+    //REDDIT SPACEPRON LAST TOP 24 POSTS
     function getRedditPhotos(){
-        $http.get('https://www.reddit.com/r/spaceporn/.json?').then(function(returnedData){
-            $scope.photos = [];
+        var returnedDataArr = [];
+        $http.get('https://www.reddit.com/r/spaceporn/.json?')
+        .then(function(returnedData){
             for (var x = 0; x <= 24; x++){
-                $scope.photos.push(returnedData.data.data.children[x]);
+                returnedDataArr.push(returnedData.data.data.children[x]);
             }
         });
+        return returnedDataArr;
     }
 }
