8bf85ffddb45ed7df36439b20cc3bfbe745fb377
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-18 00:25:20 -0400

    pretty links are up and running TODO: refresh throws 404

--- server.js
+++ server.js
@@ -7,11 +7,27 @@ var mongoose = require('mongoose'),
 	app = express();
 app.use(bodyparser.json());
 app.use(express.static(path.join(root, 'client')));
-/*app.use(express.static(path.join(root, 'node_modules')));*/
 app.use(express.static(path.join(root, 'bower_components')));
+
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
 
 app.listen(port, function(){
 	console.log(`running on port ${port}`);
 });
+
+var ignoredPaths = ['/nasa', '/reddit', '/profile/:id'];
+
+app.all('/*', function(req, res, next) {
+  if(!startsWith(req.url, ignoredPaths))
+    res.sendFile('index.html', { root: __dirname });
+  else
+    next();
+});
+
+function startsWith(string, array) {
+  for(i = 0; i < array.length; i++)
+    if(string.startsWith(array[i]))
+      return true;
+  return false;
+}
