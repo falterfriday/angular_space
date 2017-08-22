73816dfc3a904468e76408cf378584984b5e7d49
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-21 23:04:13 -0400

    migrated backend flow from server to controllers. removed original placeholder logic with specifc api calls

--- server/controllers/nasaPhotos.js
+++ server/controllers/nasaPhotos.js
@@ -1,17 +1,25 @@
 var mongoose = require('mongoose');
-const request = require('request');
-
-module.exports = new NasaPhotosController();
-
-const redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?'
+var request = require('request');
 
 function NasaPhotosController(){
-    request(redditApiUrl, function(error, response, body){
-		if(!error && response.statusCode == 200){
-			const redditResponse = JSON.parse(body);
-			return redditResponse;
-		} else {
-			console.log("Got an error: ", error, ", status code: ", response.statusCode)
-		}
-	})
-};
+    var returnedDataArr = [];
+    var nasaApiUrl;
+    var date;
+
+    for (var x = 1; x <= 2; x++){
+        date = (new Date( new Date() - 1000*60*60*24* x )
+        .toISOString())
+        .substring(0,10);            
+        nasaApiUrl = 'https://api.nasa.gov/planetary/apod?hd=True&date=' + date + '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB';
+        request(nasaApiUrl, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                const nasaResponse = JSON.parse(body);
+                returnedDataArr.push(nasaResponse);
+            } else {
+                console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`)
+            }
+        })
+    }
+    return returnedDataArr;
+}
+module.exports = new NasaPhotosController();
