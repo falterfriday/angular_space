2daac63fb291b79a65249137812d5764b1c1ca70
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-21 23:04:59 -0400

    migrated backend flow from server to controllers. incorporated placeholder api call logic. TODO

--- server/controllers/redditPhotos.js
+++ server/controllers/redditPhotos.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
-const request = require('request');
+var request = require('request');
 
-module.exports = new RedditPhotosController();
-
-const redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?'
+var redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?';
 
 function RedditPhotosController(){
-    request(nasaApiUrl, function(error, response, body){
+    request(redditApiUrl, function(error, response, body){
 		if(!error && response.statusCode == 200){
 			const redditResponse = JSON.parse(body);
 			return redditResponse;
@@ -15,3 +13,16 @@ function RedditPhotosController(){
 		}
 	})
 };
+
+module.exports = new RedditPhotosController();
+
+function getRedditPhotos(){
+        var returnedDataArr = [];
+        $http.get('https://www.reddit.com/r/spaceporn/.json?')
+        .then(function(returnedData){
+            for (var x = 0; x <= 24; x++){
+                returnedDataArr.push(returnedData.data.data.children[x]);
+            }
+        });
+        return returnedDataArr;
+    }
