6b53c7c82e8b666b81608f560e5fe17ebb5f9385
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-22 21:44:45 -0400

    removed generic api call. replaced with properly formatted call

--- server/controllers/redditPhotos.js
+++ server/controllers/redditPhotos.js
@@ -1,28 +1,21 @@
 var mongoose = require('mongoose');
 var request = require('request');
 
-var redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?';
+const redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?';
 
 function RedditPhotosController(){
+    var returnedDataArr = [];
     request(redditApiUrl, function(error, response, body){
-		if(!error && response.statusCode == 200){
-			const redditResponse = JSON.parse(body);
-			return redditResponse;
-		} else {
-			console.log("Got an error: ", error, ", status code: ", response.statusCode)
-		}
-	})
-};
-
+        if(!error && response.statusCode == 200){
+            const redditResponse = JSON.parse(body);
+            for (var x = 0; x <= 1; x++){
+                returnedDataArr.push(redditResponse.data.children[x]);
+            }  
+        } else {
+            console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`);
+        }
+    });
+    console.log("I'm in the redditPhotos controller");
+    return returnedDataArr
+}
 module.exports = new RedditPhotosController();
-
-function getRedditPhotos(){
-        var returnedDataArr = [];
-        $http.get('https://www.reddit.com/r/spaceporn/.json?')
-        .then(function(returnedData){
-            for (var x = 0; x <= 24; x++){
-                returnedDataArr.push(returnedData.data.data.children[x]);
-            }
-        });
-        return returnedDataArr;
-    }
