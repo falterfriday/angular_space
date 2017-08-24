896c3183298858ef9b7c30161bab883c407c9bde
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-23 22:10:18 -0400

    removed object export and added module export

--- server/controllers/redditPhotos.js
+++ server/controllers/redditPhotos.js
@@ -1,21 +1,27 @@
 var mongoose = require('mongoose');
 var request = require('request');
+var photoCleaner = require("../models/photo.js")
 
 const redditApiUrl = 'https://www.reddit.com/r/spaceporn/.json?';
 
-function RedditPhotosController(){
-    var returnedDataArr = [];
-    request(redditApiUrl, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            const redditResponse = JSON.parse(body);
-            for (var x = 0; x <= 1; x++){
-                returnedDataArr.push(redditResponse.data.children[x]);
-            }  
-        } else {
-            console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`);
-        }
-    });
-    console.log("I'm in the redditPhotos controller");
-    return returnedDataArr
+module.exports = {
+    getRedditPhotos: function(){
+        var returnedDataArr = [];
+        request(redditApiUrl, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                const redditResponse = JSON.parse(body);
+                for (var x = 0; x <= 1; x++){
+                    returnedDataArr.push(redditResponse.data.children[x]);
+                }  
+            } else {
+                console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`);
+            }
+        });
+        //console.log("I'm in the redditPhotos controller");
+        //console.log(returnedDataArr);
+        //return returnedDataArr
+        photoCleaner.redditPhoto(returnedDataArr);
+    }
 }
-module.exports = new RedditPhotosController();
+
+//module.exports = new getRedditPhotos();
