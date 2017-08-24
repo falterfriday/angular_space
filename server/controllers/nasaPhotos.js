4569981e0b6ad4936d3665b9e62d8cb5c2d83f87
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-23 22:09:49 -0400

    removed object export and added module export

--- server/controllers/nasaPhotos.js
+++ server/controllers/nasaPhotos.js
@@ -1,26 +1,27 @@
 var mongoose = require('mongoose');
 var request = require('request');
 
-function NasaPhotosController(){
-    var returnedDataArr = [];
-    var nasaApiUrl;
-    var date;
+module.exports = {
+     getNasaPhotos: function(){
+        var returnedDataArr = [];
+        var nasaApiUrl;
+        var date;
 
-    for (var x = 1; x <= 2; x++){
-        date = (new Date( new Date() - 1000*60*60*24* x )
-        .toISOString())
-        .substring(0,10);            
-        nasaApiUrl = 'https://api.nasa.gov/planetary/apod?hd=True&date=' + date + '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB';
-        request(nasaApiUrl, function(error, response, body){
-            if(!error && response.statusCode == 200){
-                const nasaResponse = JSON.parse(body);
-                returnedDataArr.push(nasaResponse);
-            } else {
-                console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`)
-            }
-        })
+        for (var x = 1; x <= 2; x++){
+            date = (new Date( new Date() - 1000*60*60*24* x )
+            .toISOString())
+            .substring(0,10);            
+            nasaApiUrl = 'https://api.nasa.gov/planetary/apod?hd=True&date=' + date + '&api_key=DRQGKkFd4I0cAg3uPQJHTAtd9BUjAGCDlDvZFNsB';
+            request(nasaApiUrl, function(error, response, body){
+                if(!error && response.statusCode == 200){
+                    const nasaResponse = JSON.parse(body);
+                    returnedDataArr.push(nasaResponse);
+                } else {
+                    console.log(`Got and error: ${error} \nStatus code: ${response.statusCode}`)
+                }
+            })
+        }
+        console.log("I'm in the nasaPhotos controller")
+        return returnedDataArr;
     }
-    console.log("I'm in the nasaPhotos controller")
-    return returnedDataArr;
 }
-module.exports = new NasaPhotosController();
