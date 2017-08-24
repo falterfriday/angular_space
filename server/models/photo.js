7c3ca23df2fca0feff3560c9407e28bdde1b30e9
Patrick Todd <patrick.a.todd@gmail.com>
2017-08-23 22:11:12 -0400

    created photoSchema

---
+++ server/models/photo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//this is where the json is formatted and pushed to db
+
+module.exports = {
+    redditPhoto: function(jsonArr){
+        for(let i  = 0; i < jsonArr.length; i++){
+            photoJson.url = jsonArr.url;
+            photoJson.description = jsonArr.description
+        }
+        console.log("in the photo.js file")
+        console.log(photoJson.url);
+    } 
+}
+
+let photoJson = {
+    "url": String,
+    "description": String
+}
+
+let PhotoSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+const Photo = mongoose.model('Photo', PhotoSchema);
